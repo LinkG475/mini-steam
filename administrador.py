4d16962d4fdff13521baf706522eacf5d2e12439
LinkG475 <20245365@esen.edu.sv>
2025-11-18 18:35:40 -0600

    Funciones menu

---
+++ administrador.py
@@ -0,0 +1,30 @@
+def menu_administrador():
+    while True:
+        print("\n--- MENÚ DE ADMINISTRADOR ---")
+        print("1. Registrar libro")
+        print("2. Registrar estudiante")
+        print("3. Registrar préstamo")
+        print("4. Mostrar libros")
+        print("5. Mostrar estudiantes")
+        print("6. Mostrar préstamos")
+        print("7. Salir")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            registrar_libro(lista_libros)
+        elif opcion == "2":
+            registrar_estudiante(lista_estudiantes)
+        elif opcion == "3":
+            registrar_prestamo(lista_libros, lista_estudiantes, lista_prestamos)
+        elif opcion == "4":
+            mostrar_libros(lista_libros)
+        elif opcion == "5":
+            mostrar_estudiantes(lista_estudiantes)
+        elif opcion == "6":
+            mostrar_prestamos(lista_prestamos)
+        elif opcion == "7":
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción inválida.")
